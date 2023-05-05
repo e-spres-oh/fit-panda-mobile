799417869d2204576f6c8baa39f701d9d39844cc
narcis-g <narcis_alexinho@yahoo.com>
2023-05-05 08:20:16 +0300

    Sign Up Page

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,6 +1,10 @@
-import { Routes } from './routes';
+
+
+  import { Routes } from './routes';
 
 export type RouteParams = {
   [Routes.Welcome]: undefined;
   [Routes.Login]: undefined;
+  [Routes.SignUp]: undefined;
+  [Routes.Data]: undefined;
 };
